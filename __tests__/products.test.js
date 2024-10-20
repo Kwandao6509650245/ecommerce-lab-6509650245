21be58e93be151e85a88dce06de636ad26acdd0c
Kwandao6509650245 <Kwandao.khu@dome.tu.ac.th>
2024-10-20 18:21:32 +0700

    get done

--- __tests__/products.test.js
+++ __tests__/products.test.js
@@ -5,11 +5,28 @@ describe('Product API Tests', () => {
         it('should return all products', async () => {
             const res = await request(app).get('/products');
             expect(res.status).toBe(200)
+            expect(res.body).toHaveLength(2)
             expect(res.body).toEqual([
                 { id: 1, name: 'Laptop', price: 1000, stock: 5 },
                 { id: 2, name: 'Smartphone', price: 600, stock: 10 }
             ])
         });
     });
+    describe('GET /products/:id', () => {
+        it('should return a product by ID', async () => {
+            const res = await request(app).get('/products/1');
+            expect(res.status).toBe(200)
+            expect(res.body).toEqual({ id: 1, name: 'Laptop', price: 1000, stock: 5 })
+        });
+        it('should return 404 if product not found', async () => {
+            const res = await request(app).get('/products/3');
+            expect(res.status).toBe(404)
+            expect(res.body).toEqual({ message: 'Product not found' })
+        });
+    });
+    describe('POST /products', () => {
+        it('should add a new product', async () => {
 
+        });
+    });
 });
