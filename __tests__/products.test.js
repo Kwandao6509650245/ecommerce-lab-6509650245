27b8786adfe02235b9855a0b2d45d6fd120a7003
Kwandao6509650245 <Kwandao.khu@dome.tu.ac.th>
2024-10-20 18:52:49 +0700

    PUT done

--- __tests__/products.test.js
+++ __tests__/products.test.js
@@ -12,6 +12,7 @@ describe('Product API Tests', () => {
             ])
         });
     });
+
     describe('GET /products/:id', () => {
         it('should return a product by ID', async () => {
             const res = await request(app).get('/products/1');
@@ -24,6 +25,7 @@ describe('Product API Tests', () => {
             expect(res.body).toEqual({ message: 'Product not found' })
         });
     });
+
     describe('POST /products', () => {
         it('should add a new product', async () => {
             const newProduct = {
@@ -31,12 +33,31 @@ describe('Product API Tests', () => {
                 price: 300,
                 stock: 20
             };
-
             const res = await request(app)
                 .post(`/products`)
                 .send(newProduct);
-
             expect(res.status).toBe(201)
+        });
+    });
+
+    describe('PUT /products/:id', () => {
+        it('should update an existing product', async () => {
+            const res = await request(app)
+                .put(`/products/2`)
+                .send({
+                    name: 'Iphone'
+                });
+            expect(res.status).toBe(200)
+            expect(res.body).toEqual({ id: 2, name: 'Iphone', price: 600, stock: 10 })
+        });
+        it('should return 404 if product not found', async () => {
+            const res = await request(app)
+                .put(`/products/8`)
+                .send({
+                    name: 'Airpod'
+                });
+            expect(res.status).toBe(404)
+            expect(res.body).toEqual({ message: 'Product not found' })
         });
     });
 });
